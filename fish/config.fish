52c0d0bcee2a36c8ac387884fd81b51374ccbabc
Sascha Hagedorn <mail@shagedorn.net>
2020-05-30 21:47:33 +0200

    [fish] move theme setting to local file

--- fish/config.fish
+++ fish/config.fish
@@ -6,16 +6,6 @@ if status --is-interactive
     source "$BASE16_SHELL/profile_helper.fish"
 end
 
-# Set shell theme
-if type -q base16-gruvbox-dark-soft
-    base16-gruvbox-dark-soft
-end
-
-# Set shell theme
-if type -q fish-base16-gruvbox-dark-soft
-    fish-base16-gruvbox-dark-soft
-end
-
 # FZF key bindings
 if type -q fzf_key_bindings
     fzf_key_bindings
